f3131ac9cb64e4cc7847eca420460bb603599234
Karol Antoniewicz <karol.antoniewicz@cgi.com>
2022-09-26 15:46:17 +0200

    wip

--- app/Http/Controllers/Api/GetLeistungen.php
+++ app/Http/Controllers/Api/GetLeistungen.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\LeistungNormalized;
+use DB;
 use Illuminate\Http\JsonResponse;
 
 class GetLeistungen extends Controller
 {
     public function __invoke(): JsonResponse
     {
-        return response()->json(LeistungNormalized::all());
+		$leistungen = LeistungNormalized::query()
+			->orderBy(DB::raw('ISNULL(klasse), klasse'), 'ASC')
+			->orderBy('nachname')
+			->get();
+
+        return response()->json($leistungen);
     }
 }
