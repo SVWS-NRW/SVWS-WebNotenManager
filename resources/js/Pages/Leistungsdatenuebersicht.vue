b8f995d8889852e723675b03715670970c6d3861
Karol Antoniewicz <karol.antoniewicz@cgi.com>
2023-02-13 14:58:04 +0100

    wip

--- resources/js/Pages/Leistungsdatenuebersicht.vue
+++ resources/js/Pages/Leistungsdatenuebersicht.vue
@@ -88,22 +88,22 @@
         }
 
         columns.value.length = 0
-        pushTable(true, baseColumns)
-        pushTable(true, fachlehrerColumns)
-        pushTable(true, teilleistungenColumns)
-        pushTable(true, notenColumns)
-        pushTable(true, mahnungenColumns)
-        pushTable(true, fehlstundenColumns)
-        pushTable(true, fachbezogeneBemerkungenColumns)
+        // pushTable(true, baseColumns)
+        // pushTable(true, fachlehrerColumns)
+        // pushTable(true, teilleistungenColumns)
+        // pushTable(true, notenColumns)
+        // pushTable(true, mahnungenColumns)
+        // pushTable(true, fehlstundenColumns)
+        // pushTable(true, fachbezogeneBemerkungenColumns)
 
         // Temporary switched off https://git.svws-nrw.de/phpprojekt/webnotenmanager/-/issues/101
-        // pushTable(true, baseColumns)
-        // pushTable(toggles.fachlehrer, fachlehrerColumns)
-        // pushTable(toggles.teilleistungen, teilleistungenColumns)
-        // pushTable(true, notenColumns)
-        // pushTable(toggles.mahnungen, mahnungenColumns)
-        // pushTable(true, fehlstundenColumns)
-        // pushTable(toggles.bemerkungen, fachbezogeneBemerkungenColumns)
+        pushTable(true, baseColumns)
+        pushTable(toggles.fachlehrer, fachlehrerColumns)
+        pushTable(toggles.teilleistungen, teilleistungenColumns)
+        pushTable(true, notenColumns)
+        pushTable(toggles.mahnungen, mahnungenColumns)
+        pushTable(true, fehlstundenColumns)
+        pushTable(toggles.bemerkungen, fachbezogeneBemerkungenColumns)
     }
 
     watch(toggles, (): void => drawTable())
