d425c78dada29f7f70fb61c049bffd7a2b397350
Marine-Garcia <silvia.marine-garcia@cgi.com>
2023-07-12 10:53:28 +0200

    introduce direction in table sort button component

--- resources/js/Components/TableSortButton.vue
+++ resources/js/Components/TableSortButton.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 //todo: clear up what is needed here
-//import {Leistung, Schueler} from '../types'
 import {Ref, ref, toRef, watch} from 'vue'
 import { SvwsUiIcon } from '@svws-nrw/svws-ui';
 
@@ -11,23 +10,20 @@ import { SvwsUiIcon } from '@svws-nrw/svws-ui';
         columnName: string
     }>()
 
-
-    
-    //testing here
-    const direction = ref(true);
-
+    const revertDirection = ref(false);
     const emit = defineEmits(['clicked'])
-    const clicked = (value): void => emit('clicked', value)    
+    //working version but without direction
+    const clicked = (value): void => emit('clicked', value)
+
+
 </script>
 
 <template>
-    <!-- TODO: emit to parent -->
-    <!-- <button  @click="clicked" class=""> -->
-    <!-- <button  @click="mySort(props.columnName)" class=""> -->
     <button  @click="clicked(props.columnName.toLowerCase())">
-        <span class="column-name"> {{ props.columnName}} </span>
+        <span class="column-name"> {{ props.columnName}} {{revertDirection }} </span>
         <SvwsUiIcon>
-            <mdi-arrow-down-thick class="sort-icon" v-if="props.columnName.toLowerCase() == props.sortBy"></mdi-arrow-down-thick>
+            <mdi-arrow-down-thick class="sort-icon" v-if="props.columnName.toLowerCase() == props.sortBy && revertDirection == false"></mdi-arrow-down-thick>
+            <mdi-arrow-up-thick class="sort-icon" v-else-if="revertDirection == true"></mdi-arrow-up-thick>
         </SvwsUiIcon>
     </button>
 </template>
