07b258e459c6a3226e8c143c90bae6c8373bbaf1
Silvia Marine-Garcia <silvia.marine-garcia@cgi.com>
2023-11-28 13:06:29 +0100

    adjust web routes after merges

--- routes/web.php
+++ routes/web.php
@@ -8,60 +8,54 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
 
 	//testing 2FA here
-// Route::inertia(uri: 'two-factor.login', component: 'TwoFactorAuthentication')
-// 	->name(name: 'two-factor.login')
-// 	->middleware(middleware: 'two.fa');
+	// Route::inertia(uri: 'two-factor.login', component: 'TwoFactorAuthentication')
+	// 	->name(name: 'two-factor.login')
+	// 	->middleware(middleware: 'two.fa');
 	Route::inertia(uri: '/', component: 'MeinUnterricht')
 		->name(name: 'mein_unterricht')
 		->middleware(middleware: 'mein.unterricht');
 
-    Route::inertia('impressum', 'Impressum')->name('impressum');
-    Route::inertia('datenschutz', 'Datenschutz')->name('datenschutz');
-    Route::inertia('barrierefreiheit', 'Barrierefreiheit')->name('barrierefreiheit');
+	Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function (): void {
+		Route::inertia('/', 'MeinUnterricht')
+			->name('mein_unterricht')
+			->middleware('mein.unterricht');
 
+		Route::inertia('leistungsdatenuebersicht', 'Leistungsdatenuebersicht')
+			->name('leistungsdatenuebersicht');
 
-    Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function (): void {
-	Route::inertia('/', 'MeinUnterricht')
-        ->name('mein_unterricht')
-        ->middleware('mein.unterricht');
+		Route::inertia('klassenleitung', 'Klassenleitung')
+			->name('klassenleitung')
+			->middleware('klassenleitung');
 
-	Route::inertia('leistungsdatenuebersicht', 'Leistungsdatenuebersicht')
-		->name('leistungsdatenuebersicht');
+		Route::middleware('administrator')
+			->prefix('einstellungen')
+			->name('settings.')
+			->group(function (): void {
+				Route::inertia('/', 'Settings/Matrix')->name('index');
+				Route::inertia('schule', 'Settings/School')->name('school');
+				Route::inertia('filter', 'Settings/Filter')->name('filter');
+				Route::inertia('matrix', 'Settings/Matrix')->name('matrix');
+				Route::inertia('sicherheit', 'Settings/Sicherheit')->name('sicherheit');
 
-	Route::inertia('klassenleitung', 'Klassenleitung')
-		->name('klassenleitung')
-		->middleware('klassenleitung');
+				Route::inertia('synchronisation', 'Settings/Synchronisation')->name('synchronisation');
+			});
 
-	Route::middleware('administrator')
-		->prefix('einstellungen')
-		->name('settings.')
-		->group(function (): void {
-			Route::inertia('/', 'Settings/Matrix')->name('index');
-			Route::inertia('schule', 'Settings/School')->name('school');
-			Route::inertia('filter', 'Settings/Filter')->name('filter');
-			Route::inertia('matrix', 'Settings/Matrix')->name('matrix');
-			Route::inertia('sicherheit', 'Settings/Sicherheit')->name('sicherheit');
-
-			Route::inertia('synchronisation', 'Settings/Synchronisation')->name('synchronisation');
-
-		});
-
-	Route::inertia('/filter', 'UserSettings/UserSettingsFilter')
-		->prefix('benutzereinstellungen')
-		->name('user_settings.filter');
+		Route::inertia('/filter', 'UserSettings/UserSettingsFilter')
+			->prefix('benutzereinstellungen')
+			->name('user_settings.filter');
+	});
 });
 
 Route::controller(PasswordController::class)
-	//->middleware('guest')
+	->middleware('guest')
 	->name('request_password.')
 	->group(function (): void {
 		Route::get('passwort-anfordern', 'index')->name('index');
 		Route::post('passwort-anfordern', 'execute')->name('execute');
 		Route::get('passwort-aendern/{token}', 'reset_form')->name('reset_form');
-        Route::post('passwort-aendern', 'update')->name('update');
+		Route::post('passwort-aendern', 'update')->name('update');
 	});
-});
 
-
-//Route::post('password-reset', ResetPasswordController::class)->name('password_reset');
-
+Route::inertia('impressum', 'Impressum')->name('impressum');
+Route::inertia('datenschutz', 'Datenschutz')->name('datenschutz');
+Route::inertia('barrierefreiheit', 'Barrierefreiheit')->name('barrierefreiheit');
