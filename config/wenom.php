5ad41ceeb26d8d1238ef52356761162c843a7b99
Silvia Marine-Garcia <silvia.marine-garcia@cgi.com>
2024-02-12 13:55:47 +0100

    Update npm version and version display

--- config/wenom.php
+++ config/wenom.php
@@ -2,7 +2,7 @@
 
 return [
     'version' => '0.1.2',
-    'npm' => '0.8.7',
+    'npm' => '0.8.9',
     'schulnummer' => env('SCHULNUMMER'),
     'aes_password' => env('AES_PASSWORD'),
     'aes_salt' => env('AES_SALT'),
