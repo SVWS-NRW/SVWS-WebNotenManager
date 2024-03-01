6f1d798ca158133d5e4fb4b8a456d84b4d04d918
Marine-Garcia <silvia.marine-garcia@cgi.com>
2024-03-01 14:43:18 +0100

    Update npm to 0.8.10

--- config/wenom.php
+++ config/wenom.php
@@ -2,7 +2,7 @@
 
 return [
     'version' => '0.1.2',
-    'npm' => '0.8.9',
+    'npm' => '0.8.10',
     'schulnummer' => env('SCHULNUMMER'),
     'aes_password' => env('AES_PASSWORD'),
     'aes_salt' => env('AES_SALT'),
