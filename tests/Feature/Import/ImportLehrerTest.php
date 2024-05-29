08d370e7601068cf0b094c77e4baf4167db8f5aa
Marine-Garcia <silvia.marine-garcia@cgi.com>
2024-05-29 13:04:08 +0200

    Merge feature/284-testdatenimport into Develop

--- tests/Feature/Import/ImportLehrerTest.php
+++ tests/Feature/Import/ImportLehrerTest.php
@@ -13,14 +13,9 @@ class ImportLehrerTest extends TestCase
 
     const TABLE = 'users';
 
-    /**
-     * Creates new Lehrer
-     *
-     * @return void
-     */
-    public function test_it_creates_lehrer(): void
+    private function getData(): array
     {
-        $data = json_decode('{
+        return json_decode('{
             "lehrer": [
                 {
                     "id": 155,
@@ -32,7 +27,16 @@ class ImportLehrerTest extends TestCase
                 }
             ]
         }', true);
+    }
 
+    /**
+     * Creates new
+     *
+     * @return void
+     */
+    public function test_it_creates(): void
+    {
+        $data = $this->getData();
         (new DataImportService($data))->execute();
 
         $this->assertDatabaseHas(self::TABLE, [
@@ -46,23 +50,14 @@ class ImportLehrerTest extends TestCase
     }
 
     /**
-     * Creates lehrer with random email when email is missing
+     * Creates with random email when email is missing
      *
      * @return void
      */
-    public function test_it_creates_lehrer_with_random_email_when_email_is_missing(): void
+    public function test_it_creates_with_random_email_when_email_is_missing(): void
     {
-        $data = json_decode('{
-            "lehrer": [
-                {
-                    "id": 155,
-                    "kuerzel": "ÖA",
-                    "nachname": "Ölschläger",
-                    "vorname": "Kevin",
-                    "geschlecht": "m"
-                }
-            ]
-        }', true);
+        $data = $this->getData();
+        unset($data['lehrer'][0]['eMailDienstlich']);
 
         (new DataImportService($data))->execute();
 
@@ -72,24 +67,14 @@ class ImportLehrerTest extends TestCase
     }
 
     /**
-     * Creates lehrer with random email when email was not provided
+     * Creates with random email when email was null
      *
      * @return void
      */
-    public function test_it_creates_lehrer_with_random_email_when_email_was_not_provided(): void
+    public function test_it_creates_with_random_email_when_email_was_null(): void
     {
-        $data = json_decode('{
-            "lehrer": [
-                {
-                    "id": 155,
-                    "kuerzel": "ÖA",
-                    "nachname": "Ölschläger",
-                    "vorname": "Kevin",
-                    "geschlecht": "m",
-                    "eMailDienstlich": ""
-                }
-            ]
-        }', true);
+        $data = $this->getData();
+        $data['lehrer'][0]['eMailDienstlich'] = null;
 
         (new DataImportService($data))->execute();
 
@@ -99,24 +84,31 @@ class ImportLehrerTest extends TestCase
     }
 
     /**
-     * Creates Lehrer with random email when invalid email provided
+     * Creates with random email when email was empty
      *
      * @return void
      */
-    public function test_it_creates_lehrer_with_random_email_when_invalid_email_provided(): void
+    public function test_it_creates_with_random_email_when_email_was_empty(): void
     {
-        $data = json_decode('{
-            "lehrer": [
-                {
-                    "id": 155,
-                    "kuerzel": "ÖA",
-                    "nachname": "Ölschläger",
-                    "vorname": "Kevin",
-                    "geschlecht": "m",
-                    "eMailDienstlich": "incorrect#email"
-                }
-            ]
-        }', true);
+        $data = $this->getData();
+        $data['lehrer'][0]['eMailDienstlich'] = '';
+
+        (new DataImportService($data))->execute();
+
+        $this->assertDatabaseMissing(self::TABLE, [
+            'eMailDienstlich' => ''
+        ])->assertDatabaseCount(self::TABLE, 1);
+    }
+
+    /**
+     * Creates with random email when invalid email provided
+     *
+     * @return void
+     */
+    public function test_it_creates_with_random_email_when_invalid_email_provided(): void
+    {
+        $data = $this->getData();
+        $data['lehrer'][0]['eMailDienstlich'] = 'inkorrekt#email';
 
         (new DataImportService($data))->execute();
 
@@ -126,25 +118,15 @@ class ImportLehrerTest extends TestCase
     }
 
     /**
-     * Creates Lehrer with fallback gender if incorrect gender provided
+     * Creates with fallback gender if no gender provided
      * Fallback gender is defined in \App\Models\User::class
      *
      * @return void
      */
-    public function test_it_creates_lehrer_with_fallback_gender_if_incorrect_gender_provided(): void
+    public function test_it_creates_with_fallback_gender_if_gender_not_provided(): void
     {
-        $data = json_decode('{
-            "lehrer": [
-                {
-                    "id": 155,
-                    "kuerzel": "ÖA",
-                    "nachname": "Ölschläger",
-                    "vorname": "Kevin",
-                    "geschlecht": "incorrect",
-                    "eMailDienstlich": "öa@svws-nrw.de"
-                }
-            ]
-        }', true);
+        $data = $this->getData();
+        unset($data['lehrer'][0]['geschlecht']);
 
         (new DataImportService($data))->execute();
 
@@ -156,24 +138,15 @@ class ImportLehrerTest extends TestCase
     }
 
     /**
-     * Creates Lehrer with fallback gender if no gender provided
+     * Creates with fallback gender if incorrect gender provided
      * Fallback gender is defined in \App\Models\User::class
      *
      * @return void
      */
-    public function test_it_creates_lehrer_with_fallback_gender_if_no_gender_provided(): void
+    public function test_it_creates_lehrer_with_fallback_gender_if_incorrect_gender_provided(): void
     {
-        $data = json_decode('{
-            "lehrer": [
-                {
-                    "id": 155,
-                    "kuerzel": "ÖA",
-                    "nachname": "Ölschläger",
-                    "vorname": "Kevin",
-                    "eMailDienstlich": "öa@svws-nrw.de"
-                }
-            ]
-        }', true);
+        $data = $this->getData();
+        $data['lehrer'][0]['geschlecht'] = 'Z';
 
         (new DataImportService($data))->execute();
 
@@ -200,18 +173,13 @@ class ImportLehrerTest extends TestCase
             'email' => 'mm@svws-nrw.de'
         ]);
 
-        $data = json_decode('{
-            "lehrer": [
-                {
-                    "id": 155,
-                    "kuerzel": "ÖA",
-                    "nachname": "Ölschläger",
-                    "vorname": "Kevin",
-                    "geschlecht": "m",
-                    "eMailDienstlich": "öa@svws-nrw.de"
-                }
-            ]
-        }', true);
+        $data = $this->getData();
+        $data['lehrer'][0]['id'] = 155;
+        $data['lehrer'][0]['kuerzel'] = "ÖA";
+        $data['lehrer'][0]['nachname'] = "Ölschläger";
+        $data['lehrer'][0]['vorname'] = "Kevin";
+        $data['lehrer'][0]['geschlecht'] = "m";
+        $data['lehrer'][0]['eMailDienstlich'] = "öa@svws-nrw.de";
 
         (new DataImportService($data))->execute();
 
@@ -244,25 +212,16 @@ class ImportLehrerTest extends TestCase
             'email' => 'öa@svws-nrw.de'
         ]);
 
-        $data = json_decode('{
-            "lehrer": [
-                {
-                    "id": 155,
-                    "kuerzel": "ÖA",
-                    "nachname": "Ölschläger",
-                    "vorname": "Kevin",
-                    "geschlecht": "m",
-                    "eMailDienstlich": "invalid#email.com"
-                }
-            ]
-        }', true);
+        $data = $this->getData();
+        $data['lehrer'][0]['id'] = 155;
+        $data['lehrer'][0]['eMailDienstlich'] = "invalid#email.com";
 
         (new DataImportService($data))->execute();
 
         $this->assertDatabaseHas(self::TABLE, [
             'email' => 'öa@svws-nrw.de'
         ])->assertDatabaseMissing(self::TABLE, [
-            'email' => 'invalid@email.com'
+            'email' => 'invalid#email.com'
         ])->assertDatabaseCount(self::TABLE, 1);
     }
 
@@ -278,25 +237,61 @@ class ImportLehrerTest extends TestCase
             'email' => 'öa@svws-nrw.de'
         ]);
 
-        $data = json_decode('{
-            "lehrer": [
-                {
-                    "id": 155,
-                    "kuerzel": "ÖA",
-                    "nachname": "Ölschläger",
-                    "vorname": "Kevin",
-                    "geschlecht": "m"
-                }
-            ]
-        }', true);
+        $data = $this->getData();
+        $data['lehrer'][0]['id'] = 155;
+        unset($data['lehrer'][0]['eMailDienstlich']);
 
         (new DataImportService($data))->execute();
 
-        $this->assertDatabaseHas(self::TABLE, [
+        $this->assertDatabaseHas(self::TABLE, ['email' => 'öa@svws-nrw.de'])
+            ->assertDatabaseMissing(self::TABLE, ['email' => ''])
+            ->assertDatabaseCount(self::TABLE, 1);
+    }
+
+    /**
+     * Does not update email when email is null
+     *
+     * @return void
+     */
+    public function test_it_does_not_update_email_when_email_is_null(): void
+    {
+        User::factory()->create([
+            'ext_id' => 155,
             'email' => 'öa@svws-nrw.de'
-        ])->assertDatabaseMissing(self::TABLE, [
-            'email' => ''
-        ])->assertDatabaseCount(self::TABLE, 1);
+        ]);
+
+        $data = $this->getData();
+        $data['lehrer'][0]['id'] = 155;
+        $data['lehrer'][0]['eMailDienstlich'] = null;
+
+        (new DataImportService($data))->execute();
+
+        $this->assertDatabaseHas(self::TABLE, ['email' => 'öa@svws-nrw.de'])
+            ->assertDatabaseMissing(self::TABLE, ['email' => ''])
+            ->assertDatabaseCount(self::TABLE, 1);
+    }
+
+    /**
+     * Does not update email when email is empty
+     *
+     * @return void
+     */
+    public function test_it_does_not_update_email_when_email_is_empty(): void
+    {
+        User::factory()->create([
+            'ext_id' => 155,
+            'email' => 'öa@svws-nrw.de'
+        ]);
+
+        $data = $this->getData();
+        $data['lehrer'][0]['id'] = 155;
+        $data['lehrer'][0]['eMailDienstlich'] = '';
+
+        (new DataImportService($data))->execute();
+
+        $this->assertDatabaseHas(self::TABLE, ['email' => 'öa@svws-nrw.de'])
+            ->assertDatabaseMissing(self::TABLE, ['email' => ''])
+            ->assertDatabaseCount(self::TABLE, 1);
     }
 
     /**
@@ -311,26 +306,15 @@ class ImportLehrerTest extends TestCase
             'geschlecht' => 'd'
         ]);
 
-        $data = json_decode('{
-            "lehrer": [
-                {
-                    "id": 155,
-                    "kuerzel": "ÖA",
-                    "nachname": "Ölschläger",
-                    "vorname": "Kevin",
-                    "geschlecht": "Q",
-                    "eMailDienstlich": "öa@svws-nrw.de"
-                }
-            ]
-        }', true);
+        $data = $this->getData();
+        $data['lehrer'][0]['id'] = 155;
+        $data['lehrer'][0]['geschlecht'] = 'Z';
 
         (new DataImportService($data))->execute();
 
-        $this->assertDatabaseHas(self::TABLE, [
-            'geschlecht' => 'd'
-        ])->assertDatabaseMissing(self::TABLE, [
-            'geschlecht' => 'Q'
-        ])->assertDatabaseCount(self::TABLE, 1);
+        $this->assertDatabaseHas(self::TABLE, ['geschlecht' => 'd'])
+            ->assertDatabaseMissing(self::TABLE, ['geschlecht' => 'Z'])
+            ->assertDatabaseCount(self::TABLE, 1);
     }
 
     /**
@@ -345,25 +329,61 @@ class ImportLehrerTest extends TestCase
             'geschlecht' => 'd'
         ]);
 
-        $data = json_decode('{
-            "lehrer": [
-                {
-                    "id": 155,
-                    "kuerzel": "ÖA",
-                    "nachname": "Ölschläger",
-                    "vorname": "Kevin",
-                    "eMailDienstlich": "öa@svws-nrw.de"
-                }
-            ]
-        }', true);
+        $data = $this->getData();
+        $data['lehrer'][0]['id'] = 155;
+        unset($data['lehrer'][0]['geschlecht']);
 
         (new DataImportService($data))->execute();
 
-        $this->assertDatabaseHas(self::TABLE, [
+        $this->assertDatabaseHas(self::TABLE, ['geschlecht' => 'd'])
+            ->assertDatabaseMissing(self::TABLE, ['geschlecht' => ''])
+            ->assertDatabaseCount(self::TABLE, 1);
+    }
+
+    /**
+     * Does not update gender when gender is NULL
+     *
+     * @return void
+     */
+    public function test_it_does_not_update_gender_when_gender_is_null(): void
+    {
+        User::factory()->create([
+            'ext_id' => 155,
             'geschlecht' => 'd'
-        ])->assertDatabaseMissing(self::TABLE, [
-            'geschlecht' => ''
-        ])->assertDatabaseCount(self::TABLE, 1);
+        ]);
+
+        $data = $this->getData();
+        $data['lehrer'][0]['id'] = 155;
+        $data['lehrer'][0]['geschlecht'] = null;
+
+        (new DataImportService($data))->execute();
+
+        $this->assertDatabaseHas(self::TABLE, ['geschlecht' => 'd'])
+            ->assertDatabaseMissing(self::TABLE, ['geschlecht' => ''])
+            ->assertDatabaseCount(self::TABLE, 1);
+    }
+
+    /**
+     * Does not update gender when gender is empty
+     *
+     * @return void
+     */
+    public function test_it_does_not_update_gender_when_gender_is_empty(): void
+    {
+        User::factory()->create([
+            'ext_id' => 155,
+            'geschlecht' => 'd'
+        ]);
+
+        $data = $this->getData();
+        $data['lehrer'][0]['id'] = 155;
+        $data['lehrer'][0]['geschlecht'] = '';
+
+        (new DataImportService($data))->execute();
+
+        $this->assertDatabaseHas(self::TABLE, ['geschlecht' => 'd'])
+            ->assertDatabaseMissing(self::TABLE, ['geschlecht' => ''])
+            ->assertDatabaseCount(self::TABLE, 1);
     }
 
     /**
